7a5405e645b962631a0c47494fa0ada11b729779
DiegoAlejandroGarzon <tingonay10@hotmail.com>
2024-08-01 14:48:41 -0500

    sistema de roles y permisos

---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Crear permisos
+        Permission::create(['name' => 'edit articles']);
+        Permission::create(['name' => 'delete articles']);
+        Permission::create(['name' => 'publish articles']);
+        Permission::create(['name' => 'unpublish articles']);
+
+        // Crear roles y asignar permisos
+        $role = Role::create(['name' => 'writer']);
+        $role->givePermissionTo('edit articles');
+
+        $role = Role::create(['name' => 'admin']);
+        $role->givePermissionTo(['edit articles', 'delete articles', 'publish articles', 'unpublish articles']);
+    }
+}
