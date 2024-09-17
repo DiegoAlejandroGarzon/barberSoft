ded28e5003cc95c4996da2c5f80f5aca27349cd4
DiegoAlejandroGarzon <tingonay10@hotmail.com>
2024-09-17 15:47:49 -0500

    EVE209 agregar estado de pago en pdf del ticket

--- resources/views/pdf/assistant.blade.php
+++ resources/views/pdf/assistant.blade.php
@@ -13,6 +13,28 @@
 <body>
     <h1>{{ $evento->name }}</h1>
     <p><strong>Fecha del evento:</strong> {{ $evento->event_date }}</p>
+
+    <div class="mt-5">
+        <div class="">
+            @if ($asistente->is_paid)
+                <div role="alert" class="alert rounded-md bg-success text-slate-900 dark:border-success box p-5">
+                    <H1>ESTADO DEL PAGO DEL TICKET: </H1>Pagado
+                </div>
+            @else
+                @if ($asistente->totalPayments() == 0)
+                <div role="alert" class="alert relative border rounded-md bg-danger border-danger text-white dark:border-danger mb-2 box p-5">
+                    <H1>ESTADO DEL TICKET DEL TICKET: </H1>No Pagado
+
+                </div>
+                @else
+                <div role="alert" class="alert rounded-md bg-warning text-slate-900 dark:border-warning box p-5">
+                    <H1>ESTADO DEL TICKET DEL TICKET: </H1>Pendiente
+                </div>
+                @endif
+            @endif
+        </div>
+    </div>
+
     <hr>
     <h2>Información del Asistente</h2>
     <table border="1">
