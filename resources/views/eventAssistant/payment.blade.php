1ab52fc2fac88af4dd9b73ffc17f36a8a223faa9
DiegoAlejandroGarzon <tingonay10@hotmail.com>
2024-09-27 09:02:15 -0500

    boton ir listado asistentes en modulo pago y qr

--- resources/views/eventAssistant/payment.blade.php
+++ resources/views/eventAssistant/payment.blade.php
@@ -5,6 +5,7 @@
 @endsection
 
 @section('subcontent')
+    <a href="{{ route('eventAssistant.index', ['idEvent' => $eventAssistant->event_id]) }}">volver</a>
     <h2 class="intro-y mt-10 text-lg font-medium">Informacion del Pago</h2>
     <div class="mt-5 flex justify-center">
         <a class="text-info box p-3" href="{{ route('eventAssistant.sendEmailInfoPago', ['id' => $eventAssistant->id]) }}" target="_blank">
